14fd907ddc485f4b714b51cb63fe97beb2182d24
Aswin Reji <aswinreji.004@gmail.com>
2024-08-08 16:55:11 +0000

    TypeScript Basics

---
+++ basics.ts
@@ -0,0 +1,15 @@
+const num : number = 12
+
+const user : string = 'Hello World'
+
+const username = 'Arshu'
+
+function greeting(name:string):string {
+  return `Good evening ${name}`
+}
+
+const greets = greeting(username)
+
+console.log(greets);
+
+
