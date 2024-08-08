42f15c3686f17e477bd2d08f60c1bda680bfc304
Aswin Reji <aswinreji.004@gmail.com>
2024-08-08 18:19:39 +0000

    Typescript basics using Interface to make the structure of object

--- basics.ts
+++ basics.ts
@@ -12,4 +12,38 @@ const greets = greeting(username)
 
 console.log(greets);
 
+// Defining Types
+// We are creating an object with an inferred type which includes name:string and id:number
+const userDetail = {
+  name: "Arshu",
+  id:0,
+}
+
+
+// Then we can explicitly describe this object's shape using an Interface declaration
+
+interface User {
+  name: string;
+  id: number;
+}
+
+// In this case the if ojbect doesn't match the interface we have provided , TypeScript will warn us
+const userDetailWithInterface: User= {
+ name: "Justin",
+ id: 0,
+}
+
+
+class UserAccount {
+  name: string;
+  id: number;
+  constructor(name: string, id: number) {
+   this.name = name;
+   this.id = id;
+  }
+}
+
+const User: User = new UserAccount("Murphy",1)
+console.log(User,'User is showing');
+
 
